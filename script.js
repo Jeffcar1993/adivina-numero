9fe2478729b988d35d42545292dc50b05529d1e2
Jeffcar1993 <jphoenix.07@hotmail.com>
2024-03-21 15:57:09 -0500

    juego de adivinanza

---
+++ script.js
@@ -0,0 +1,37 @@
+// aca el juego selecciona un numero al azar entre 1 y 100 redondeado al que esta mas abajo y le suma 1 
+
+let numeroAzar = Math.floor(Math.random()*100) + 1
+
+let numeroEntrada = document.getElementById("numeroEntrada")
+let mensaje = document.getElementById("mensaje")
+
+let intento = document.getElementById("intento")
+let intentos = 0
+
+// accion se ejecuta cuanto se da click en comprobar
+
+function chequearResultado (){
+
+        intentos ++
+        intento.textContent = intentos
+
+    let numeroIngresado = parseInt(numeroEntrada.value)
+
+    if (numeroIngresado < 1 || numeroIngresado > 100 || isNaN (numeroIngresado)){
+        mensaje.textContent = "Porfavor introduce un número valido entre 1 y 100";
+        mensaje.style.color = "red";
+            return
+    }
+
+    if (numeroIngresado === numeroAzar){
+        mensaje.textContent = " Felicitaciones adivinaste el número";
+        mensaje.style.color = "green";
+        numeroEntrada.disabled = true ;
+    } else if (numeroIngresado < numeroAzar){
+        mensaje.textContent = " Mas alto, el número es mayor al que digitaste";
+        mensaje.style.color = "red";
+    } else {
+        mensaje.textContent = " Mas bajo, el número es menor al que digitaste";
+        mensaje.style.color = "red";
+    }
+}
